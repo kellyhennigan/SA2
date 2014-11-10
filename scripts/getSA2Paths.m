06e41b101f097509efef18d6c6c86f797a4e14ed
kellyhennigan <kelhennigan@gmail.com>
2014-11-09 22:36:09 -0800

    updated getSA2Paths function

--- scripts/getSA2Paths.m
+++ scripts/getSA2Paths.m
@@ -1,14 +1,14 @@
-function expPaths = getSA2Paths(subject)
+function p = getSA2Paths(subject)
 % --------------------------------
 % usage: get a structural array containing all relevant paths for project
 % SA2
 % 
 % INPUT:
-%   subject - subject id string (optional)
+%   subject (optional) - subject id string
 
 % 
 % OUTPUT:
-%   expPaths - structural array containing relevant paths
+%   p - structural array containing relevant paths
 % 
 % 
 % author: Kelly, kelhennigan@gmail.com, 09-Nov-2014
@@ -20,29 +20,25 @@ if strcmp(cName,'dn0a221879.sunet')     % laptop
     baseDir = '/Users/Kelly/SA2/data';
 elseif strcmp(cName,'psy-jal-ml.stanford.edu') % lab desktop
     baseDir = '/Users/kelly/SA2/data';
-else 
-expPaths = struct();
+elseif strcmp(cName,'mt-tamalpais') % mt-tam server
+    baseDir = '/home/kelly/SA2/data';
+end
 
-expPaths.baseDir = baseDir;
-expPaths.subj        = fullfile(baseDir, subject);  % subject directory
+p = struct();
+
+p.baseDir = baseDir;
 
 % subject directories
-% expPaths.afni        = fullfile(expPaths.subj, 'afni/');
-expPaths.design_mats = fullfile(expPaths.subj, 'design_mats/');
-% expPaths.dti96trilin = fullfile(expPaths.subj, 'dti96trilin/');
-% expPaths.fibers      = fullfile(expPaths.subj, 'fibers/');
-% expPaths.raw         = fullfile(expPaths.subj, 'raw/');
-% expPaths.raw_func    = fullfile(expPaths.subj, 'raw_func/');
-% expPaths.raw_dti     = fullfile(expPaths.subj, 'raw_dti/');
-expPaths.regs        = fullfile(expPaths.subj, 'regs/');
-expPaths.results     = fullfile(expPaths.subj, 'results/');
-% expPaths.results_hab = fullfile(expPaths.subj, 'results_hab/');
-expPaths.ROIs        = fullfile(expPaths.subj, 'ROIs/');
-expPaths.stimtimes   = fullfile(expPaths.subj, 'stimtimes/');
-expPaths.slicetimes  = fullfile(expPaths.subj, 'slicetimes/');
-% expPaths.funcROIs    = fullfile(expPaths.subj, 'funcROIs/');
-expPaths.t1          = fullfile(expPaths.subj, 't1/');
-
+if ~notDefined(subject)
+    p.subj = fullfile(baseDir, subject);  % subject directory
+    p.design_mats = fullfile(p.subj, 'design_mats/');
+    p.raw         = fullfile(p.subj, 'raw/');
+    p.regs        = fullfile(p.subj, 'regs/');
+    p.ROIs        = fullfile(p.subj, 'ROIs/');
+    p.stimtimes   = fullfile(p.subj, 'stimtimes/');
+    p.t1          = fullfile(p.subj, 't1/');
+end
+    
 end
 
 
